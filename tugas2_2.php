2218bcbc42eda23c8b9b1824f0dcf8074a4a0c4a
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-10-07 12:01:32 +0700

    Add files via upload

---
+++ tugas2_2.php
@@ -0,0 +1,17 @@
+<?php
+$beli = 50000;
+$jual= 80000;
+$keuntungan = $jual - $beli;
+$persentase = ($keuntungan / $beli)*100;
+
+echo "Jual = ", $jual;
+echo "<br>Beli = ", $beli;
+echo "<br>Keuntungan = ?";
+
+
+
+echo "<br><br>Jawaban : ", $persentase, "%";
+
+
+
+?>
